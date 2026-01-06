b970eed0b9c0f5be22295dffc12079c6d5fab698
Greg Young <greg.young147@gmail.com>
2026-01-06 13:35:52 -0500

    Initial Commit

---
+++ Versant-Movie-Row-Project/Versant_Movie_Row_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Versant_Movie_Row_ProjectApp.swift
+//  Versant-Movie-Row-Project
+//
+//  Created by Gregory Young on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Versant_Movie_Row_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
